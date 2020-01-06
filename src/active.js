1fb7dcb9f014b43bd1078396370746dd9ce6eb0c
Kevin Schweikert <kevin@kevinschweikert.de>
2020-01-06 15:48:10 +0100

    Prettier styling

--- src/active.js
+++ src/active.js
@@ -1,4 +1,4 @@
-import { writable } from 'svelte/store'
+import { writable } from "svelte/store";
 
-export const activeElement = writable()
-export const noChange = writable(false)
+export const activeElement = writable();
+export const noChange = writable(false);
--- src/configs.js
+++ src/configs.js
@@ -1,19 +1,26 @@
-import {writable} from 'svelte/store'
-const path = require('path')
+import { writable } from "svelte/store";
+const path = require("path");
 
 export const configs = writable([]);
 
-export const sessionPath = writable("")
+export const sessionPath = writable("");
 
-export const cfgFileName = writable("")
+export const cfgFileName = writable("project_config.json");
 
-export const createConfig = (projectPath, additionalFiles = [], description = "") => {
-    return {
-        id: Math.random().toString(36).substr(2,5) + Date.now().toString(36),
-        path: path.basename(projectPath),
-        project: path.basename(projectPath),
-        state: "angebot",
-        desc: description,
-        files: additionalFiles,
-    }
-}
+export const createConfig = (
+  projectPath,
+  additionalFiles = [],
+  description = ""
+) => {
+  return {
+    id:
+      Math.random()
+        .toString(36)
+        .substr(2, 5) + Date.now().toString(36),
+    path: path.basename(projectPath),
+    project: path.basename(projectPath),
+    state: "angebot",
+    desc: description,
+    files: additionalFiles
+  };
+};
