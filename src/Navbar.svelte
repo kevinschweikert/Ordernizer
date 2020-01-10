eefaada5f307bff504779bfd29b8502d1c417e22
Kevin Schweikert <kevin@kevinschweikert.de>
2020-01-10 13:23:00 +0100

    Added files for responsive design

---
+++ src/Navbar.svelte
@@ -0,0 +1,26 @@
+<script>
+  import ThemeToggle from "./ThemeToggle.svelte";
+  import AddProject from "./AddProject.svelte";
+  import Filemanager from "./Filemanager.svelte";
+</script>
+
+<div class="navbar">
+  <ThemeToggle />
+  <div class="spacer" />
+  <Filemanager />
+  <AddProject />
+
+</div>
+
+<style>
+  .navbar {
+    height: 100px;
+    margin-bottom: 10px;
+    padding: 10px;
+    border-radius: var(--roundness-big);
+    background-color: var(--light-bg);
+    display: grid;
+    grid-gap: 5px;
+    grid-template-columns: 1fr auto 1fr 6vw;
+  }
+</style>
